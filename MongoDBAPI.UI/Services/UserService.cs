de4f3b6835303737702e3938d5cd31c7f8d6c431
unknown <mauriciomelendez678@outlook.com>
2024-11-21 22:45:27 -0600

    v1

---
+++ MongoDBAPI.UI/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBAPI.UI.Services
+{
+    public class UserService
+    {
+        private bool _isAuthenticated = false;
+
+        public bool IsAuthenticated => _isAuthenticated;
+
+        public void Login(string username, string password)
+        {
+            if (username == "admin" && password == "123")
+            {
+                _isAuthenticated = true;
+            }
+        }
+
+        public void Logout()
+        {
+            _isAuthenticated = false;
+        }
+    }
+}
